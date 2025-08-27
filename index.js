43f0ecbcd36a7f1a112f8e540717c82accf05672
mick811 <mikkelelvers@outlook.com>
2025-08-27 09:58:17 +0200

    loops is easier :(

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const car = {
 
 console.log("Brand:", car.brand); // should return "Brand: Toyota" -> dot notation
 console.log("Brand:", car["brand"]) // same as above but with brackets notation
+
+// loops through the car object and logs the key and value because I CAN!!!
+console.log(" \nLooping through the car object:");
+for(const key in car) {
+    console.log(`${key}: ${car[key]}`);
+}
