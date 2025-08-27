93dab2ae66ecb91fb28bff9346563466ba8eaf44
mick811 <mikkelelvers@outlook.com>
2025-08-27 09:56:15 +0200

    test car object dot/bracket

---
+++ index.js
@@ -0,0 +1,10 @@
+// made a car object because i'm far too lazy
+const car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: "Red",
+}
+
+console.log("Brand:", car.brand); // should return "Brand: Toyota" -> dot notation
+console.log("Brand:", car["brand"]) // same as above but with brackets notation
